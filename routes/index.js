5a4cdde14f288b24cd7d9b7aaf97bdef5fd8cf14
Thibault-d <thibaultdelfaud@gmail.com>
2020-03-11 21:40:20 +0100

    finalised the Venues views. I will now work on the connexion with Spotify API

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,17 @@ router.get('/artists-results', function(req, res, next) {
 router.get('/artist-details', function(req, res, next) {
   res.render('artists/artist-details');
 });
+
+router.get('/venues', function(req, res, next) {
+  res.render('venues/venues-browse');
+});
+
+router.get('/venue-events', function(req, res, next) {
+  res.render('venues/venue-events');
+});
+
+router.get('/venue-tickets', function(req, res, next) {
+  res.render('venues/venue-tickets');
+});
+
 module.exports = router;
