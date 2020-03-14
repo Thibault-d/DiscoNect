5c9979d64f618843f0c947a29b9710b7b47c3282
Thibault-d <thibaultdelfaud@gmail.com>
2020-03-14 18:35:13 +0100

    added a template of map, installed mapbox

--- routes/venues.js
+++ routes/venues.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const SpotifyWebApi = require('spotify-web-api-node');
+var MapboxClient = require('mapbox');
 
-router.get('/', function (req, res, next) { 
-    res.render("venues/venues-browse")
-});
+var client = new MapboxClient(process.env.MAPBOX_TOKEN);
+
+router.get('/', function (req, res, next) {
+   res.render("venues/venues-browse")});
 
 module.exports = router;
