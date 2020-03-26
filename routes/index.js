db709d4eba1b070dc2a69d3dd2923672bb817434
Thibault-d <thibaultdelfaud@gmail.com>
2020-03-26 23:25:13 +0100

    Did a very basic design for  the menu and the homepage

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    style: 'index.css'
+  })
 });
 
 module.exports = router;
