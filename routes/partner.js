98161d2d85e5efb5976f583fd8b01f21fda82948
alejandro <alejimenezgitgithub@gmail.com>
2020-03-26 20:06:00 +0100

    diferents changes

--- routes/partner.js
+++ routes/partner.js
@@ -3,6 +3,8 @@ var router    = express.Router();
 const Venue   = require('../models/Venue');
 const Event   = require('../models/Event');
 
+console.log('aaaaaaaaaaaaaaaaaa');
+
 // VIEW THE FORM FOR CREATE A VENUE IF YOU ARE A PARTNER
 router.get('/', function (req, res, next) {
   res.render('partner/create-venue');
@@ -10,7 +12,7 @@ router.get('/', function (req, res, next) {
 
 // VIEW THE FORM FOR CREATE A EVENT IF YOU ARE A PARTNER
 router.get('/create-event', function (req, res, next) {
-    res.render('partner/create-venue');
+    res.render('partner/create-event');
   });
 
 module.exports = router;
