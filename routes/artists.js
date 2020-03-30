a3c1c66bbee6a7385d30e00a7d5c8cf0306de004
Thibault-d <thibaultdelfaud@gmail.com>
2020-03-30 19:29:21 +0200

    push my code

--- routes/artists.js
+++ routes/artists.js
@@ -73,11 +73,19 @@ router.get("/details/:id", (req, res) => {
        })
     ]).then((data) =>{
         const select = data[1].body.tracks.slice(0,3);
+        let events = data[2];
+        let empty = "";
+        console.log(events);
+        if(events.length == 0) {
+            empty = "Sorry, there are currently no events featuring this artist"
+            console.log(empty);
+        }
         res.render("artists/artists-details", {
             genre: data[0].body.genres,
             details: data[0].body,
             tracks: select,
-            event:data[2]
+            event: events,
+            empty: empty
         });    
     })
     .catch(error => { return error })
